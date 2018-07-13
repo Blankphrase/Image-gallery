869dffce8c503e261c6b251e04cede3117c045e4
Blankphrase <ckasera6486@gmail.com>
2018-07-13 12:11:24 +0300

    creation of models

--- image/models.py
+++ image/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    location = models.ForeignKey('Location', null=True)
+    path = models.ImageField(upload_to = 'gallery/')
+    category = models.ForeignKey('Category', null = True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
