b730e46a45d0d7e9c5f587382e055e183fd0e43f
Blankphrase <ckasera6486@gmail.com>
2018-07-14 08:53:53 +0300

    adding media templates to settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'image.apps.ImageConfig',
-    'bootstrap3'
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
