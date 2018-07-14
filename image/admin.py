32a61f66a6def5a63310e227e264f5d2d36830f9
Blankphrase <ckasera6486@gmail.com>
2018-07-14 08:52:17 +0300

    registering models

--- image/admin.py
+++ image/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Gallery,Location,Category
+
+
+from .models import Gallerie,Location,Categorie
 
 # Register your models here.
 
-admin.site.register(Gallery)
+admin.site.register(Gallerie)
 admin.site.register(Location)
-admin.site.register(Category)
+admin.site.register(Categorie)
