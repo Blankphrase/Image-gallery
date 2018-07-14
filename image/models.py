48e571f52da1ca04f1088ffdd8eb2c7d5bd26998
Blankphrase <ckasera6486@gmail.com>
2018-07-14 08:51:00 +0300

    adding a column to gallery model

--- image/models.py
+++ image/models.py
@@ -2,19 +2,42 @@ from django.db import models
 
 # Create your models here.
 
-class Gallery(models.Model):
-    title = models.CharField(max_length = 255)
-    description = models.TextField()
-    location = models.ForeignKey('Location', null=True)
-    path = models.ImageField(upload_to = 'gallery/')
-    category = models.ForeignKey('Category', null = True)
-    pub_date = models.DateTimeField(auto_now_add=True)
-
-
 
 class Location(models.Model):
+    
     name = models.CharField(max_length=255)
 
 
-class Category(models.Model):
+class Categorie(models.Model):
+    
     name = models.CharField(max_length=255)
+
+
+class Gallerie(models.Model):
+    
+    title = models.CharField(max_length = 255)
+    
+    description = models.TextField()
+    
+    location = models.ForeignKey(Location, null=True)
+    
+    path = models.ImageField(upload_to = 'gallery/')
+    
+    category = models.ForeignKey(Categorie, null = True)
+    
+    pub_date = models.DateTimeField(auto_now_add=True)
+  
+    def __str__(self):
+  	    
+        return self.title
+
+    def save_images(self):
+
+        self.save() 
+
+    def delete_images(self):
+        
+        self.delete()
+
+        
+
